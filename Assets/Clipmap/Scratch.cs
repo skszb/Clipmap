89773e1b927c1d95f65b08611e6cbb6a7d4a1efb
Zhanbo <zhanbolin5@gmail.com>
2024-09-25 18:38:37 +0800

    Experiment with Assets Loading

--- Assets/Clipmap/Scratch.cs
+++ Assets/Clipmap/Scratch.cs
@@ -1,18 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.IO;
+using Unity.Collections;
+using Unity.IO.LowLevel.Unsafe;
+using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
 
-public class Scratch : MonoBehaviour
+class AsyncReadSample : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private ReadHandle readHandle;
+    NativeArray<ReadCommand> cmds;
+    // VVVVVVVVVVVVVVV Optional, for profiling VVVVVVVVVVVVVVV
+    string assetName = "myfile";
+    ulong typeID = 114; // from ClassIDReference
+    AssetLoadingSubsystem subsystem = AssetLoadingSubsystem.Scripts;
+    // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    public unsafe void Start()
     {
-        
+        string filePath = Path.Combine(Application.streamingAssetsPath, "myfile.bin");
+        cmds = new NativeArray<ReadCommand>(1, Allocator.Persistent);
+        ReadCommand cmd;
+        cmd.Offset = 0;
+        cmd.Size = 1024;
+        cmd.Buffer = (byte*)UnsafeUtility.Malloc(cmd.Size, 16, Allocator.Persistent);
+        cmds[0] = cmd;
+        readHandle = AsyncReadManager.Read(filePath, (ReadCommand*)cmds.GetUnsafePtr(), 1, assetName, typeID, subsystem);
     }
 
-    // Update is called once per frame
-    void Update()
+    public unsafe void Update()
     {
-        
+        if (readHandle.IsValid() && readHandle.Status != ReadStatus.InProgress)
+        {
+            Debug.LogFormat("Read {0}", readHandle.Status == ReadStatus.Complete ? "Successful" : "Failed");
+            readHandle.Dispose();
+            UnsafeUtility.Free(cmds[0].Buffer, Allocator.Persistent);
+            cmds.Dispose();
+        }
     }
 }
